e3b672b5dd8e6f63507e047545ba767548c8bc1c
ElTioGonza <gonz230300@gmail.com>
2021-07-14 13:44:31 -0300

    Ahora la pagina /contact cada card (que tenga su respectivo nombre de social media en LIB/SOCIAL MEDIA) te envia a una pagina del OWNER y sino por defecto a Facebook

--- components/Card.js
+++ components/Card.js
@@ -1,21 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp, faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons"
 import { faMailBulk, faPhone, faMobile, faFax, faMap, faSmoking } from "@fortawesome/free-solid-svg-icons"
+import { facebook, instagram, whatsapp } from "../lib/socialMedia";
 
 import React from 'react'
+import Link from "next/link";
 
 function Card({ title, socialMedia, description, color, last }) {
 
   const icon = getIcons(socialMedia)
   const colorSelected = getColors(color)
   const textColor = "text-gray-500"
+  const defaultLink = "https://facebook.com";
+  const ownerSocialMedia = { facebook, whatsapp: whatsapp.owner.askForService(""), instagram }
 
   return (
-    <article className={`flex flex-col px-2 pt-8 pb-16 ${!last && "border-b border-gray-300"} sm:border sm:border-gray-300  sm:px-4 sm:rounded`}>
-      <FontAwesomeIcon className={`text-8xl ${colorSelected} self-center my-8`} icon={icon} />
-      <h3 className="pb-3 text-3xl font-bold">{title}</h3>
-      <div className={`py-2 ${textColor}`} dangerouslySetInnerHTML={{ __html: description }} />
-    </article>
+    <Link href={ownerSocialMedia[socialMedia] ?? defaultLink}>
+      <a>
+        <article className={`flex flex-col px-2 pt-8 pb-16 ${!last && "border-b border-gray-300"} sm:border sm:border-gray-300  sm:px-4 sm:rounded`}>
+          <FontAwesomeIcon className={`text-8xl ${colorSelected} self-center my-8`} icon={icon} />
+          <h3 className="pb-3 text-3xl font-bold">{title}</h3>
+          <div className={`py-2 ${textColor} sm:h-32`} dangerouslySetInnerHTML={{ __html: description }} />
+        </article>
+      </a>
+    </Link>
   )
 }
 
