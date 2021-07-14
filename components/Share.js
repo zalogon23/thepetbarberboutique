f7070d45d30a33dad4f6f9cf2508882fb6d3ccca
ElTioGonza <gonz230300@gmail.com>
2021-07-14 11:14:32 -0300

    Volvimos responsive el COMPONENTE 'SHARE'

--- components/Share.js
+++ components/Share.js
@@ -6,17 +6,17 @@ import React from 'react'
 function Share({ url }) {
   return (
     <section>
-      <h3 className="px-2 py-8 uppercase font-bold text-center">Si te gusto el contenido te invitamos a que lo compartas con tus amigos y familiares:</h3>
-      <div className="grid grid-cols-3">
-        <div className="py-4 px-2 text-sm bg-blue-800 flex text-white justify-center items-center">
+      <h3 className="px-2 py-8 uppercase font-bold text-center md:text-2xl">Si te gusto el contenido te invitamos a que lo compartas con tus amigos y familiares:</h3>
+      <div className="grid grid-cols-3 text-sm sm:text-xl cursor-pointer">
+        <div className="hover:bg-blue-700 py-4 px-2 bg-blue-800 flex text-white justify-center items-center">
           <FontAwesomeIcon className="mr-2" icon={faFacebook} />
           <FacebookShareButton url={url}>Compartir</FacebookShareButton>
         </div>
-        <div className="py-4 px-2 text-sm bg-green-600 flex text-white justify-center items-center">
+        <div className="hover:bg-green-500 py-4 px-2 bg-green-600 flex text-white justify-center items-center">
           <FontAwesomeIcon className="mr-2" icon={faWhatsapp} />
           <WhatsappShareButton url={url}>Mostrar</WhatsappShareButton>
         </div>
-        <div className="py-4 px-2 text-sm bg-blue-500 flex text-white justify-center items-center">
+        <div className="hover:bg-blue-400 py-4 px-2 bg-blue-500 flex text-white justify-center items-center">
           <FontAwesomeIcon className="mr-2" icon={faFacebookMessenger} />
           <FacebookMessengerShareButton url={url}>Enviar</FacebookMessengerShareButton>
         </div>
