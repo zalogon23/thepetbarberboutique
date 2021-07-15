352b6cd8b77bb04c69cad3a8f6286be9ea45d20f
ElTioGonza <gonz230300@gmail.com>
2021-07-15 08:07:21 -0300

    El HEADER ya es responsive

--- components/Header.js
+++ components/Header.js
@@ -16,17 +16,17 @@ function Header() {
           </Link>
           <h1 className="text-2xl font-aclo mx-2 text-blue-500 font-black text-center"><Link href="/">THE PET BARBER BOUTIQUE</Link></h1>
         </div>
-        <div className="bg-blue-500 py-2 w-full flex flex-col items-center text-white text-2xl cursor-pointer" onClick={toggleOpen}>
+        <div className="bg-blue-500 py-2 w-full flex flex-col items-center text-white text-2xl cursor-pointer h-16 md:hidden" onClick={toggleOpen}>
           <FontAwesomeIcon className="text-5xl transition hover:scale-125" icon={faBone} />
         </div>
       </div>
-      <nav className={`bg-blue-600 transition-height duration-150 overflow-hidden ${open ? "h-60" : "h-0"}`}>
-        <ul className={`w-full h-full flex flex-col justify-around py-3 px-5 transition-transform duration-700 transform ${open ? "translate-x-0 opacity-100" : "-translate-x-32 opacity-0"} text-xl font-bold text-white`}>
-          <li className="hover:text-blue-400"><Link href="/"><a>Inicio</a></Link></li>
-          <li className="hover:text-blue-400"><Link href="/servicios"><a>Servicios</a></Link></li>
-          <li className="hover:text-blue-400"><Link href="/contacto"><a>Contacto</a></Link></li>
-          <li className="hover:text-blue-400"><Link href="/nosotros"><a>Nosotros</a></Link></li>
-          <li className="hover:text-blue-400"><Link href="/blog"><a>BLOG</a></Link></li>
+      <nav className={`bg-blue-600 transition-height duration-150 overflow-hidden ${open ? "h-60" : "h-0"} md:bg-blue-500 md:h-16`}>
+        <ul className={`w-full h-full flex flex-col justify-around py-3 px-5 transition-transform duration-700 transform ${open ? "translate-x-0 opacity-100" : "-translate-x-32 opacity-0"} text-xl font-bold text-white md:flex-row md:translate-x-0 md:justify-end md:opacity-100`}>
+          <li className="hover:text-blue-400 flex items-center md:px-6"><Link href="/"><a>Inicio</a></Link></li>
+          <li className="hover:text-blue-400 flex items-center md:px-6"><Link href="/servicios"><a>Servicios</a></Link></li>
+          <li className="hover:text-blue-400 flex items-center md:px-6"><Link href="/contacto"><a>Contacto</a></Link></li>
+          <li className="hover:text-blue-400 flex items-center md:px-6"><Link href="/nosotros"><a>Nosotros</a></Link></li>
+          <li className="hover:text-blue-400 flex items-center md:px-6"><Link href="/blog"><a>BLOG</a></Link></li>
         </ul>
       </nav>
     </header>
