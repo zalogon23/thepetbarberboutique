b2c6ab880b9a846e9b008e7d3300cacaf772aa92
ElTioGonza <gonz230300@gmail.com>
2021-07-16 08:49:44 -0300

    Heroku responsive

--- components/Article.js
+++ components/Article.js
@@ -6,14 +6,15 @@ function Article({ title, formats, links, keywords = "pet barber boutique perros
   const backgroundColor = alternative ? " bg-blue-800 " : " bg-white"
   const bottomBorder = last ? "" : "border-b border-gray-300"
   const buttonFeatures = alternative ? "bg-blue-800 text-white border-white hover:bg-white hover:text-blue-800" : "hover:bg-blue-800 hover:text-white border-blue-800 bg-white text-blue-800"
+  console.log(keywords)
 
   return (
     <article className={`flex flex-col items-center overflow-hidden pb-12 break-words md:px-4 ${bottomBorder} ${backgroundColor}`}>
       <h2 style={{ lineHeight: "1.2em" }} className={`text-center sm:max-w-lg sm:px-0 md:text-6xl md:max-w-4xl ${titleColor} py-8 px-3 text-2xl font-aclo font-bold md:py-24 ${!formats && "pb-4 md:py-12"}`}>{title}</h2>
       <div className={`md:flex ${alternative ? "md:flex-row-reverse" : "md:flex-row"} items-start`}>
-        {formats && <img className="md:rounded sm:max-w-lg md:max-w-xs lg:max-w-md w-full md:mt-3" src={formats.medium.url || formats.small.url} alt={keywords} />}
-        <div style={{ lineHeight: "1.5em" }} className={`text-lg px-3 py-5 sm:max-w-lg sm:px-0 sm:text-xl md:text-2xl md:pb-16 md:pt-0 md:max-w-xl md:px-12 lg:px-16 ${paragraphColor}`} dangerouslySetInnerHTML={{ __html: content }} />
-        {/* `<img className="md:rounded sm:max-w-lg md:max-w-xs lg:max-w-md w-full md:mt-3" src=${formats.medium.url || formats.small.url} alt=${keywords} /> ${content}`}} /> */}
+        {/* {formats && <img className="md:rounded sm:max-w-lg md:max-w-xs lg:max-w-md w-full md:mt-3" src={formats.medium.url || formats.small.url} alt={keywords} />} */}
+        <div style={{ lineHeight: "1.5em" }} className={`text-lg px-3 py-5 sm:max-w-lg sm:px-0 sm:text-xl md:text-2xl md:pb-16 md:pt-0 md:max-w-full md:px-12 lg:px-16 ${paragraphColor}`} dangerouslySetInnerHTML={{ __html: /*content }} />*/
+         `<img class="md:rounded sm:max-w-lg md:max-w-xs md:float-left md:mr-8 lg:max-w-md w-full md:mt-3" src="${formats.medium.url || formats.small.url}" alt="${keywords}" /> ${content}`}} />
       </div>
       {
         links &&
